71e512e4f08b773499bed75c34df41572380e97c
OXPHOS <engelzora@gmail.com>
2016-04-21 12:57:07 -0400

    hw1-1

--- Homework1/Iris.R
+++ Homework1/Iris.R
@@ -1,33 +1,46 @@
 #install.packages("randomForest")
 library(randomForest)
-setwd('~/Desktop/Homework1/')
-iris
-print(summary(iris))
-#table(iris$Species)
+setwd('~/Github/BiomedicalMachineLearning/Homework1/')
 
+# Get info of Iris, set categories and feature matrix
+summary(iris)
 X = iris[, 1:4]
 Y = iris$Species
 
-rf = list(list(randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 10)),
-          list(randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 50)),
-          list(randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 100)))
-rf1 = randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 10)
-rf2 = randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 50)
-rf3 = randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 100)
+# Try different parameter for randomForest
+# rf = list(list(randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 10)),
+#           list(randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 50)),
+#           list(randomForest(x=X, y=Y, ntree=200, do.tract=T, sampsize = 100)))
+rf1 = randomForest(x=X, y=Y, ntree=200, sampsize = 10)
+rf2 = randomForest(x=X, y=Y, ntree=200, sampsize = 50)
+rf3 = randomForest(x=X, y=Y, ntree=200, sampsize = 100)
+rf4 = randomForest(x=X, y=Y, ntree=40, sampsize = 100)
+rf5 = randomForest(x=X, y=Y, ntree=200, sampsize = 100)
+rf6 = randomForest(x=X, y=Y, ntree=1000, sampsize = 100)
 
-par(mfrow=c(2, 3))
-plot(rf1)
-legend("topright", colnames(rf1$err.rate), cex=1, text.width = 60, fill=1:4)
-plot(rf2)
-legend("topright", colnames(rf1$err.rate), cex=1, text.width = 60, fill=1:4)
-plot(rf3)
-legend("topright", colnames(rf1$err.rate), cex=1, text.width = 60, fill=1:4)
+# Plot results
+png(filename="Iris-simulation.png", width = 960, height = 1920, units = "px", pointsize = 24)
 
-#barplot(t(importance(rf1)), col=4)
-#barplot(t(importance(rf2)), col=4)
-#barplot(t(importance(rf3)), col=4)
+par(mfrow=c(4, 3))
+
+plot(rf1, sub="Tree=200,SampleSize=10", ylim=c(0, 0.3), lwd=2)
+legend("topright", colnames(rf1$err.rate), cex=1, inset=0.03, fill=1:4)
+plot(rf2, sub="Tree=200,SampleSize=50", ylim=c(0, 0.3), lwd=2)
+legend("topright", colnames(rf2$err.rate), cex=1, inset=0.03, fill=1:4)
+plot(rf3, sub="Tree=200,SampleSize=100", ylim=c(0, 0.3), lwd=2)
+legend("topright", colnames(rf3$err.rate), cex=1, inset=0.03, fill=1:4)
 varImpPlot(rf1)
 varImpPlot(rf2)
 varImpPlot(rf3)
 
+plot(rf4, sub="Tree=40,SampleSize=100", ylim=c(0, 0.2), lwd=2)
+legend("topright", colnames(rf4$err.rate), cex=1, inset=0.03, fill=1:4)
+plot(rf5, sub="Tree=200,SampleSize=100", ylim=c(0, 0.2), lwd=2)
+legend("topright", colnames(rf5$err.rate), cex=1, inset=0.03, fill=1:4)
+plot(rf6, sub="Tree=1000,SampleSize=100", ylim=c(0, 0.2), lwd=2)
+legend("topright", colnames(rf6$err.rate), cex=1, inset=0.03, fill=1:4)
+varImpPlot(rf4)
+varImpPlot(rf5)
+varImpPlot(rf6)
 
+dev.off()
--- Homework1/Nuclei.R
+++ Homework1/Nuclei.R
@@ -1,34 +1,132 @@
-setwd("~/Desktop/Homework1")
-spot1 = read.csv("spot1.csv", header=T, sep=';')
-#image(matrix(as.numeric(spot1[1, 6:ncol(spot1)]), nrow=60))
+#install.packages("randomForest")
+#install.packages("pROC")
+library(randomForest)
+library(pROC)
+png(filename="Nuclei-simulation.png", width = 1200, height = 960, units = "px", pointsize = 24)
+setwd('~/Github/BiomedicalMachineLearning/Homework1/')
+spot1 = read.csv("spot1.csv", header = T, sep = ';')
+par(mfrow = c(2, 3))
+image(matrix(as.numeric(spot1[1, 6:ncol(spot1)]), nrow = 60),
+      main = "Visulization of one sample")
 
-### Extract categories, features and generate preliminary feature matrix
-Y = spot1[, "Labeler1"][1:100]
+
+#######################################################################
+###########          Extract categories and features         ##########
+###########        generate preliminary feature matrix       ##########
+#######################################################################
+Y = spot1[, "Labeler1"]
 Y = as.factor(Y)
 X = c()
-for (i in 1:100) { #nrow(spot1)) {
+for (i in 1:nrow(spot1)) {
   X = rbind(X, spot1[i, 6:ncol(spot1)])
 }
 
-### Repeat randomForest simulation
-### Enrich significant features
-importance = array(0, dim=c(ncol(spot1) - 5, 1))
-for (i in 1:100) {
-  rf_raw = randomForest(x=X, y=Y, ntree=200, do.tract=T)
-  importance = rf_raw$importance + importance
+
+#######################################################################
+#######    Explore different tree heights and sample sizes      #######
+#######################################################################
+repeats = 12
+height.list = c(150, 80, 40, 20)
+nodenum.list = c(10, 40, 100)
+err.rate.summary = c()
+for (i in 1:repeats) {
+  rf.raw = randomForest(x = X, y = Y, ntree = 400, sampsize = height.list[i %% 4 + 1], 
+                        maxnode = nodenum.list[i %% 3 + 1])
+  err.rate.summary = rbind(err.rate.summary, colMeans(
+                     rf.raw$err.rate[(nrow(rf.raw$err.rate)-51) : (nrow(rf.raw$err.rate)-1), ]))
 }
-plot(rf_raw)
-legend("topright", colnames(rf_raw$err.rate), cex=1, text.width = 40, fill=1:3)
-importance = t(importance)
- 
-importance_sort = order(importance, decreasing=T)
-barplot(importance[importance_sort])
+err.rate.summary
+### Pick condition 5: sample size = 80, maxNode = default max.
+
+
+#######################################################################
+#######             Repeat randomForest simulation              #######
+#######               Enrich significant features               #######
+#######################################################################
+
+repeats = 50
+importance = array(0, dim = c(ncol(X), 1))
+
+# Run simulation
+for (i in 1:repeats) {
+  set.seed(i)
+  rf.raw = randomForest(x = X, y = Y, ntree = 300, sampsize = 80)
+  importance = rf.raw$importance + importance
+  plot(rf.raw, ylim = c(0, 1), lwd = 1, main = "randomForest error rate with all features")
+  par(new = TRUE)
+}
+legend("topright", colnames(rf.raw$err.rate), cex = 1, text.font = 10, inset = 0.03, fill = 1:3)
+par(new = FALSE)
+
+# Sort and plot ranked feature importance 
+importance = t(importance) / repeats
+importance.sort = order(importance, decreasing = T)[1:300]
+plot(importance[importance.sort], type='h', main="Importance barplot of the \n300 features with highest values")
 
 # Generate trimmed feature matrix
-X_trim = X[, top[1:200]]
-rf = randomForest(x=X_trim, y=Y, ntree=200, do.tract=T)
-plot(rf)
-legend("topright", colnames(rf$err.rate), cex=1, text.width = 40, fill=1:3)
+X.trim = X[, importance.sort[1:100]]
+rf.trim = randomForest(x = X.trim, y = Y, ntree = 300)
+plot(rf.trim, ylim = c(0, 1), lwd = 2, main = "randomForest model error rate with\nfirst 100 features with highest values")
+legend("topright", colnames(rf$err.rate), cex = 1, text.font = 10, inset = 0.03, fill = 1:3)
 
-barplot(t(importance(rf)))
+#######################################################################
+########     Train randomForest only with quantile features     #######
+#######################################################################
 
+# Run randomForest using only 0%, 25%, 75%, 100% quantile features
+X.quantile = t(apply(X, 1, quantile, probs = seq(0, 1, 0.25)))
+rf.quantile = randomForest(x = X.quantile, y = Y, ntree = 300)
+#plot(rf.quantile, ylim = c(0, 1), lwd = 2)
+#legend("topright", colnames(rf$err.rate), cex = 1, text.font = 10, inset = 0.03, fill = 1:3)
+barplot(t(rf.quantile$importance), main="Importance barplot of quantile features")
+X.quantile = cbind(X, X.quantile[, 2:4])
+
+
+#######################################################################
+#######            Include quantile as new features             #######
+#######    Explore different tree heights and sample sizes      #######
+#######################################################################
+repeats = 12
+height.list = c(150, 80, 40, 20)
+nodenum.list = c(10, 40, 100)
+err.rate.summary.quantile = c()
+for (i in 1:repeats) {
+  rf.raw.quantile = randomForest(x = X.quantile, y = Y, ntree = 400, sampsize = height.list[i %% 4 + 1], 
+                    maxnode = nodenum.list[i %% 3 + 1])
+  err.rate.summary.quantile = rbind(err.rate.summary.quantile, colMeans(
+         rf.raw.quantile$err.rate[(nrow(rf.raw.quantile$err.rate)-51) : (nrow(rf.raw.quantile$err.rate)-1), ]))
+}
+err.rate.summary.quantile
+### Pick condition 5: sample size = 80, maxNode = default max.
+
+#######################################################################
+#######             Repeat randomForest simulation              #######
+#######               Enrich significant features               #######
+#######################################################################
+
+repeats = 50
+importance.quantile = array(0, dim=c(ncol(X.quantile), 1))
+
+# Run simulation
+for (i in 1:repeats) {
+  set.seed(i)
+  rf.raw.quantile = randomForest(x=X.quantile, y=Y, ntree=300, sampsize=80)
+  importance.quantile = rf.raw.quantile$importance + importance.quantile
+  #plot(rf.raw.quantile, ylim=c(0, 1), lwd=1)
+  #par(new = TRUE)
+}
+#legend("topright", colnames(rf.raw.quantile$err.rate), cex=1, text.font=10, inset=0.03, fill=1:3)
+#par(new = FALSE)
+
+# Sort and plot ranked feature importance 
+importance.quantile = t(importance.quantile) / repeats
+importance.sort.quantile = order(importance, decreasing = T)
+
+# Generate trimmed feature matrix with quantiles
+X.trim.quantile = X.quantile[, importance.sort.quantile[1:100]]
+rf.trim.quantile = randomForest(x = X.trim.quantile, y = Y, ntree = 300)
+plot(rf.trim.quantile, ylim = c(0, 1), lwd = 2, 
+     main = "randomForest model error rate with\nthe 100 features with highest values")
+legend("topright", colnames(rf$err.rate), cex = 1, text.font = 10, inset = 0.03, fill = 1:3)
+
+dev.off()
